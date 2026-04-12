8c40708f860f688ddef727b7a302eb084e043f54
Kevin Hase <hasekevin21@googlemail.com>
2026-04-12 16:45:09 +0200

    add order create serializer from offer detail

--- orders_app/api/serializers.py
+++ orders_app/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from offers_app.models import OfferDetail
 from orders_app.models import Order
 
 
@@ -22,6 +23,73 @@ class OrderReadSerializer(serializers.ModelSerializer):
 			'created_at',
 			'updated_at',
 		]
+
+
+class OrderCreateSerializer(serializers.ModelSerializer):
+	"""Create an order snapshot from a selected offer detail."""
+
+	offer_detail_id = serializers.PrimaryKeyRelatedField(
+		source='offer_detail',
+		queryset=OfferDetail.objects.select_related('offer__user'),
+		write_only=True,
+	)
+
+	class Meta:
+		model = Order
+		fields = [
+			'id',
+			'offer_detail_id',
+			'customer_user',
+			'business_user',
+			'title',
+			'revisions',
+			'delivery_time_in_days',
+			'price',
+			'features',
+			'offer_type',
+			'status',
+			'created_at',
+			'updated_at',
+		]
+		read_only_fields = [
+			'id',
+			'customer_user',
+			'business_user',
+			'title',
+			'revisions',
+			'delivery_time_in_days',
+			'price',
+			'features',
+			'offer_type',
+			'status',
+			'created_at',
+			'updated_at',
+		]
+
+	def create(self, validated_data):
+		offer_detail = validated_data.pop('offer_detail')
+		customer_user = validated_data.pop('customer_user', None)
+
+		if customer_user is None:
+			request = self.context.get('request')
+			if request and request.user and request.user.is_authenticated:
+				customer_user = request.user
+
+		if customer_user is None:
+			raise serializers.ValidationError(
+				{'offer_detail_id': 'Authenticated customer user is required.'}
+			)
+
+		return Order.objects.create(
+			customer_user=customer_user,
+			business_user=offer_detail.offer.user,
+			title=offer_detail.title,
+			revisions=offer_detail.revisions,
+			delivery_time_in_days=offer_detail.delivery_time_in_days,
+			price=offer_detail.price,
+			features=offer_detail.features,
+			offer_type=offer_detail.offer_type,
+		)
 		read_only_fields = [
 			'id',
 			'customer_user',
