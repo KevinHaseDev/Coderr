cfceeb10f328d76454314b1f0efb98a7a3fb7af4
Kevin Hase <hasekevin21@googlemail.com>
2026-04-12 16:43:59 +0200

    add order read serializer

---
+++ orders_app/api/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+
+from orders_app.models import Order
+
+
+class OrderReadSerializer(serializers.ModelSerializer):
+	"""Serialize full order response payload for read endpoints."""
+
+	class Meta:
+		model = Order
+		fields = [
+			'id',
+			'customer_user',
+			'business_user',
+			'title',
+			'revisions',
+			'delivery_time_in_days',
+			'price',
+			'features',
+			'offer_type',
+			'status',
+			'created_at',
+			'updated_at',
+		]
+		read_only_fields = [
+			'id',
+			'customer_user',
+			'business_user',
+			'title',
+			'revisions',
+			'delivery_time_in_days',
+			'price',
+			'features',
+			'offer_type',
+			'status',
+			'created_at',
+			'updated_at',
+		]
