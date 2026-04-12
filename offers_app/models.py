a45ae7e1c0c75084f6d08809d0d5d8207435e256
Kevin Hase <hasekevin21@googlemail.com>
2026-04-12 13:25:46 +0200

    add model offer

--- offers_app/models.py
+++ offers_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User = get_user_model()
+
+class Offer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='offers')
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='offer_images/')
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class OfferDetail(models.Model):
+    offer = models.OneToOneField(Offer, on_delete=models.CASCADE, related_name='detail')
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    delivery_time = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.offer.title} - {self.title}"
