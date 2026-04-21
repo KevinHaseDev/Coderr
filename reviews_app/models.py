a3fa9a704e8787919e49a0c602e3ee320d39dbd4
Kevin Hase <hasekevin21@googlemail.com>
2026-04-21 08:52:20 +0200

    review model + migration

--- reviews_app/models.py
+++ reviews_app/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Review(models.Model):
+	business_user = models.ForeignKey(
+		User,
+		on_delete=models.CASCADE,
+		related_name='received_reviews',
+	)
+	reviewer = models.ForeignKey(
+		User,
+		on_delete=models.CASCADE,
+		related_name='written_reviews',
+	)
+	rating = models.PositiveSmallIntegerField(
+		validators=[MinValueValidator(1), MaxValueValidator(5)],
+	)
+	description = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		constraints = [
+			models.UniqueConstraint(
+				fields=['business_user', 'reviewer'],
+				name='unique_business_user_reviewer_review',
+			),
+		]
+
+	def __str__(self):
+		return f'Review #{self.pk}'
