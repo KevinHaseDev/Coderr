e45522b96be0b20b5af6a331dc58b82a80c4c722
Kevin Hase <hasekevin21@googlemail.com>
2026-04-21 09:02:23 +0200

    add review create endpoint behavior

--- reviews_app/api/views.py
+++ reviews_app/api/views.py
@@ -1,17 +1,26 @@
 from rest_framework.exceptions import ValidationError
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 
+from reviews_app.api.permissions import IsCustomerUser
 from reviews_app.api.serializers import ReviewSerializer
 from reviews_app.models import Review
 
 
-class ReviewsAppStatusView(ListAPIView):
+class ReviewsAppStatusView(ListCreateAPIView):
 	"""List reviews for authenticated users with optional filtering and ordering."""
 
 	permission_classes = [IsAuthenticated]
 	serializer_class = ReviewSerializer
 	queryset = Review.objects.all()
+
+	def get_permissions(self):
+		if self.request.method == 'POST':
+			return [IsCustomerUser()]
+		return [IsAuthenticated()]
+
+	def perform_create(self, serializer):
+		serializer.save(reviewer=self.request.user)
 
 	def get_queryset(self):
 		queryset = self._get_filtered_queryset()
